97fc4e471171b930fe09061b3bf57622b53c94f2
CodeHead99 <akshitasg08@gmail.com>
2022-11-20 19:21:27 +0530

    experience component

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import './About.css'
-import ME from '../../assets/me-about.jpg'
-import {FaAward} from 'react-icons/fa'
-import {FiUsers} from 'react-icons/fi'
-import {VscFolderLibrary} from 'react-icons/vsc'
+import React from "react";
+import "./About.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Get to Know</h5>
       <h2>About Me</h2>
-      
-       <div className=".container about__container">
+
+      <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
             <img src={ME} alt="About Image" />
@@ -18,29 +18,37 @@ const About = () => {
         </div>
         <div className="about__content">
           <div className="about__cards">
-            <article className='about__card'>
-              <FaAward className='about__icon'/>
+            <article className="about__card">
+              <FaAward className="about__icon" />
               <h5>Clients</h5>
               <small>200+ WorldWide</small>
             </article>
-            <article className='about__card'>
-              <FiUsers className='about__icon'/>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
               <h5>Projects</h5>
               <small>80+ Projects</small>
             </article>
-            <article className='about__card'>
-              <VscFolderLibrary className='about__icon'/>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
               <h5>Experience</h5>
               <small>1 year Working</small>
             </article>
           </div>
 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius facilis officiis qui ratione blanditiis omnis voluptates, voluptatibus amet. Temporibus, explicabo. Commodi inventore perferendis suscipit. Ad tempora cumque asperiores inventore excepturi.</p>
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+            facilis officiis qui ratione blanditiis omnis voluptates,
+            voluptatibus amet. Temporibus, explicabo. Commodi inventore
+            perferendis suscipit. Ad tempora cumque asperiores inventore
+            excepturi.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
-       </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import "./Header.css"
-import CTA from './CTA'
-import ME from '../../assets/me.png'
-import HeaderSocial from './HeaderSocial'
+import React from "react";
+import "./Header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
+import HeaderSocial from "./HeaderSocial";
 const Header = () => {
   return (
     <header>
-        <div className="container header_container">
-            <h5>Hello I'm</h5>
-            <h1>Akshita</h1>
-            <h5 className='text-light'>Full Stack Developer</h5>
-            <CTA/>
-            <HeaderSocial/>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Akshita</h1>
+        <h5 className="text-light">Full Stack Developer</h5>
+        <CTA />
+        <HeaderSocial />
 
-            <div className="me">
-                <img src={ME} alt="me" />
-            </div>
-
-            <a href="#contact" className='scroll__down'>Scroll Down</a>
-            
+        <div className="me">
+          <img src={ME} alt="me" />
         </div>
-    </header>
-  )
-}
 
-export default Header
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
