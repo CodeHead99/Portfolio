cde0225ace2a26f579cb25a11b8eadcebc5030c8
CodeHead99 <akshitasg08@gmail.com>
2022-11-25 00:45:10 +0530

    completed code

--- portfolio/src/components/Contact/Contact.jsx
+++ portfolio/src/components/Contact/Contact.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
-import "./Contact.css"
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./Contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa";
 const Contact = () => {
-  return (
-    <section id='contact'>Contact</section>
-  )
-}
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-export default Contact
+    emailjs
+      .sendForm(
+        "service_k4p3fiq",
+        "template_9n917ih",
+        form.current,
+        "cKNpw2zSMcy5fX53i"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>dumyemail@gmail.com</h5>
+            <a href="mailto:akshita.2507.sharma@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <FaLinkedin className="contact__option-icon" />
+            <h4>Linkedin</h4>
+            <h5>Akshita Sharma</h5>
+            <a
+              href="https://www.linkedin.com/in/sharma-akshita/"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+918773456857 </h5>
+            <a
+              href="https://api.whatsapp.com/send?phone+8773456857"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <header>
       <div className="container header_container">
         <h5>Hello I'm</h5>
-        <h1>Akshita</h1>
+        <h1>Akshita Sharma</h1>
         <h5 className="text-light">Full Stack Developer</h5>
         <CTA />
         <HeaderSocial />
